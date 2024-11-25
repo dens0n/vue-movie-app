0c0f3e47553510b7712585b595e8b11f9728456e
dens0n <156082076+dens0n@users.noreply.github.com>
2024-11-25 19:38:59 +0100

    update App component; implement search query and category selection functionality, pass props to Navbar and Movies components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue'
 import Movies from './components/Movies.vue'
+import { ref } from 'vue'
 
+const searchQuery = ref<string>('')
+const selectedCategory = ref<string>('trending')
+
+
+const updateSearchQuery = (query: string) => {
+    searchQuery.value = query
+}
+
+const updateCategory = (category: string) => {
+    selectedCategory.value = category
+}
 </script>
 
 <template>
-    <Navbar />
-    <Movies />
+    <Navbar @search="updateSearchQuery" @categoryChange="updateCategory" />
+    <Movies :searchQuery="searchQuery" :selectedCategory="selectedCategory" />
 </template>
