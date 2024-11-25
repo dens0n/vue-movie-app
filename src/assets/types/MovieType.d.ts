93e972438cf7d1fa5d0b6b05eae15456131dc1fd
dens0n <156082076+dens0n@users.noreply.github.com>
2024-11-25 16:43:31 +0100

    add MovieType interface; define structure for media items in TypeScript

---
+++ src/assets/types/MovieType.d.ts
@@ -0,0 +1,18 @@
+export default interface MediaItem {
+    backdrop_path: string // Path to the backdrop image
+    id: number // Unique identifier for the media item
+    name?: string // Name of the media item
+    title?: string // title of the movie
+    original_name: string // Original name of the media item
+    overview: string // Summary or description of the media item
+    poster_path: string // Path to the poster image
+    media_type: string // Type of media (e.g., "tv", "movie")
+    adult: boolean // Indicates if the media is for adults
+    original_language: string // Original language code (e.g., "en")
+    genre_ids: number[] // Array of genre IDs
+    popularity: number // Popularity score
+    first_air_date: string // Date of first airing
+    vote_average: number // Average vote rating
+    vote_count: number // Total number of votes
+    origin_country: string[] // List of origin countries
+}
