32b52bf27cbcb73395168b8b4d288b40d7d396ff
dens0n <156082076+dens0n@users.noreply.github.com>
2024-11-26 18:19:58 +0100

    update App component; refactor search query and category handling, enhance event emission for category changes, and improve data binding with v-model

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,26 @@
 import Navbar from './components/Navbar.vue'
 import Movies from './components/Movies.vue'
 import { ref } from 'vue'
+import type { category } from './assets/types/MovieType'
 
 const searchQuery = ref<string>('')
-const selectedCategory = ref<string>('trending')
-
+const selectedCategory = ref<category>('trending')
 
 const updateSearchQuery = (query: string) => {
     searchQuery.value = query
+    console.log('(App.vue) search query:', searchQuery.value)
 }
 
-const updateCategory = (category: string) => {
+const updateCategory = (category: category) => {
     selectedCategory.value = category
+    console.log('(App.vue) selected category:', selectedCategory.value)
 }
 </script>
 
 <template>
-    <Navbar @search="updateSearchQuery" @categoryChange="updateCategory" />
-    <Movies :searchQuery="searchQuery" :selectedCategory="selectedCategory" />
+    <Navbar @search="updateSearchQuery" @category-change="updateCategory" />
+    <Movies
+        v-model:searchQuery="searchQuery"
+        :selectedCategory="selectedCategory"
+    />
 </template>
